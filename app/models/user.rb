3faf40f988a3799403230aecf92750739b4943be
Rysth <johnpalacios.t@gmail.com>
2023-11-14 14:56:57 -0500

    Generate Users' model, controller, and view.

---
+++ app/models/user.rb
@@ -0,0 +1,6 @@
+class User < ApplicationRecord
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable, :confirmable
+  has_many :foods, dependent: :destroy
+  has_many :recipes, dependent: :destroy
+end

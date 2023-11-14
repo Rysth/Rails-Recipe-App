75b5f9d0873737f05f8640fd32c8b3d85283001a
Rysth <johnpalacios.t@gmail.com>
2023-11-14 14:45:14 -0500

    Add seeds.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,41 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+# Create some users
+5.times do
+  User.create!(
+    name: Faker::Name.name,
+    email: Faker::Internet.email,
+    password: 'password',
+    password_confirmation: 'password'
+  )
+end
+
+# Create some foods for each user
+User.all.each do |user|
+  5.times do
+    user.foods.create!(
+      name: Faker::Food.ingredient,
+      measurement_unit: Faker::Measurement.metric_volume,
+      price: rand(1..10)
+    )
+  end
+end
+
+# Create some recipes for each user
+User.all.each do |user|
+  3.times do
+    recipe = user.recipes.create!(
+      name: Faker::Food.dish,
+      preparation_time: rand(10..60),
+      cooking_time: rand(10..60),
+      description: Faker::Food.description,
+      public: [true, false].sample
+    )
+
+    # Add some foods to each recipe
+    3.times do
+      recipe.recipe_foods.create!(
+        food: user.foods.sample,
+        quantity: rand(1..5)
+      )
+    end
+  end
+end

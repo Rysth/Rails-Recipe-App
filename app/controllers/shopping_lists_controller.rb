8e8aeaccb3df63295a8ec8515474eb329104c9c6
Rebelzob <karimdavid07@gmail.com>
2023-11-15 20:02:03 +0000

    Add shopping list functionality to recipe controller

---
+++ app/controllers/shopping_lists_controller.rb
@@ -0,0 +1,14 @@
+class ShoppingListsController < ApplicationController
+  def index
+    @recipe = Recipe.find(params[:recipe_id])
+    @recipe_foods = @recipe.recipe_foods.includes(:food)
+
+    @shopping_list = []
+    total_price = 0
+
+    @recipe_foods.each do |recipe_food|
+    @shopping_list << { name: recipe_food.food.name, quantity: recipe_food.quantity, measurement_unit: recipe_food.food.measurement_unit, price: recipe_food.food.price }
+    total_price += recipe_food.food.price
+    end
+  end
+end

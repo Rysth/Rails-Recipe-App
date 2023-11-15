6df3712681f5c534e73aaf8dd76c52306515156a
Rebelzob <karimdavid07@gmail.com>
2023-11-15 20:19:25 +0000

    Refactor shopping list and recipe food specs

--- app/controllers/shopping_lists_controller.rb
+++ app/controllers/shopping_lists_controller.rb
@@ -7,8 +7,9 @@ class ShoppingListsController < ApplicationController
     total_price = 0
 
     @recipe_foods.each do |recipe_food|
-    @shopping_list << { name: recipe_food.food.name, quantity: recipe_food.quantity, measurement_unit: recipe_food.food.measurement_unit, price: recipe_food.food.price }
-    total_price += recipe_food.food.price
+      @shopping_list << { name: recipe_food.food.name, quantity: recipe_food.quantity,
+                          measurement_unit: recipe_food.food.measurement_unit, price: recipe_food.food.price }
+      total_price += recipe_food.food.price
     end
   end
 end

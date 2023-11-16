9aad60c537ce0aac5d6c7ade0d56b4bb4d9456a9
Rysth <johnpalacios.t@gmail.com>
2023-11-16 14:15:16 -0500

    Improve n+1 eager loading by using recipe_foods[:food] in Public.

--- app/controllers/public_controller.rb
+++ app/controllers/public_controller.rb
@@ -2,6 +2,6 @@ class PublicController < ApplicationController
   skip_before_action :authenticate_user!, only: %i[index]
 
   def index
-    @recipes = Recipe.includes(:user).where(public: true).all
+    @recipes = Recipe.includes(:user, recipe_foods: [:food]).where(public: true).all
   end
 end

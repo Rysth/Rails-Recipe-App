a74f6870960a74c52d5cd5e134ca8abaa4adbcd7
Rysth <johnpalacios.t@gmail.com>
2023-11-16 14:40:24 -0500

    Enhance form creation for Foods#create.

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -21,7 +21,7 @@ class FoodsController < ApplicationController
 
     respond_to do |format|
       if @food.save
-        format.html { redirect_to food_url(@food), notice: 'Food was successfully created.' }
+        format.html { redirect_to foods_path, notice: 'Food was successfully created.' }
       else
         format.html { render :new, status: :unprocessable_entity }
       end

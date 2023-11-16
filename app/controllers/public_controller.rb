c7f81445c3b71e0fb6caf3f7bf8512d66ce0167e
Rysth <johnpalacios.t@gmail.com>
2023-11-16 13:50:38 -0500

    Fix n+1 problems in Public/index.

--- app/controllers/public_controller.rb
+++ app/controllers/public_controller.rb
@@ -2,6 +2,6 @@ class PublicController < ApplicationController
   skip_before_action :authenticate_user!, only: %i[index]
 
   def index
-    @recipes = Recipe.where(public: true).all
+    @recipes = Recipe.includes(:user).where(public: true).all
   end
 end

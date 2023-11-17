2dddfa9e31afc884e00f054a5f646cf23c65434b
John Palacios <johnpalacios.t@gmail.com>
2023-11-17 17:51:37 -0500

    Create new view for Recipes.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,5 +1,5 @@
 class RecipesController < ApplicationController
-  before_action :set_recipe, only: %i[new show edit update destroy]
+  before_action :set_recipe, only: %i[show edit update destroy]
   before_action :authenticate_user!, except: %i[public show]
 
   # GET /recipes or /recipes.json
